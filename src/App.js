55c3706a8b5790823b5b6be824221ba610b09964
shoibaldas <shoibalshantu@gmail.com>
2022-10-11 22:46:14 +0600

    Quiz topics are loaded from Api with loader in Homepage

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
         [
           {
             path: '/',
+            loader: () => fetch('https://openapi.programming-hero.com/api/quiz'),
             element: <Home></Home>
           },
           {
--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const Topics = () => {
+const Topics = ({ topic }) => {
+    const { id, logo, name, total } = topic;
     return (
-        <div>
-            
+        <div className="flex flex-col transition duration-300 bg-white rounded shadow-sm hover:shadow">
+            <div className="relative w-full h-64">
+                <img src={logo} className="object-cover w-full h-full bg-black rounded-t" alt="Plan" />
+            </div>
+            <div className="flex flex-col justify-between flex-grow p-8 border border-t-0 rounded-b">
+                <div className='mb-4'>
+                    <div className="text-lg font-semibold">{name}</div>
+                    <p className="text-sm text-gray-900">
+                        Total Quiz: {total}
+                    </p>
+                </div>
+                <a
+                    href="/"
+                    className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-violet-900 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                >
+                    Start Quiz
+                </a>
+            </div>
         </div>
     );
 };

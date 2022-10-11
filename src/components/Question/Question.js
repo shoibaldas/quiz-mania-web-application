0193750df4b45360c2729f48809ed9da21189443
shoibaldas <shoibalshantu@gmail.com>
2022-10-12 02:29:35 +0600

    Question answer matching right and wrong answer completed

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 const Question = ({ quiz }) => {
     const { question, options, correctAnswer } = quiz;
-
+    const handleButtonClick = (ans) => {
+        if (JSON.stringify(ans) === JSON.stringify(correctAnswer)) {
+            toast.success('The Answer is correct!', { autoClose: 1000 })
+        }
+        else {
+            toast.error('Your Answer is wrong! Try Again.', { autoClose: 1000 })
+        }
+    }
     return (
         <div
             className="max-w-screen-md flex rounded-lg border border-violet-900 shadow-md bg-gray-200">
             <div className="bg-white p-12 rounded-lg shadow-lg w-full">
                 <h5 className="mb-4 text-xl font-semibold tracking-tight text-violet-900">{question.slice(3, -4)}</h5>
-                <div class="flex items-center justify-center mb-3">
+                <div className="flex items-center justify-center mb-3">
                     <div class="flex flex-col gap-4" role="group">
                         {
-                            options.map((pt, idx) => <button type="button" className="block mt-2 border border-violet-900 rounded-lg py-2 px-6 hover:bg-violet-400 cursor-pointer">{pt}</button>)
+                            options.map((input, idx) => <button key={idx} onClick={(event) => handleButtonClick(event.target.innerText)}  className="block mt-2 border border-violet-900 rounded-lg py-2 px-6 hover:bg-violet-400 cursor-pointer">{input}</button>)
                         }
                     </div>
                 </div>
@@ -20,6 +28,5 @@ const Question = ({ quiz }) => {
 
     );
 };
-{/* <label for="a" class=""><input id="a" type="radio" class="hidden" value="a" disabled=""> Bags</label> */ }
 
 export default Question;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <>
+    <ToastContainer position='top-center' />
+
     <App />
-  </React.StrictMode>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function

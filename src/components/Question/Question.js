db601c2746e4182db1104a29c2c809331979ffb4
shoibaldas <shoibalshantu@gmail.com>
2022-10-12 07:37:20 +0600

    Quiz container restyled and scoreboard has been added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { toast } from 'react-toastify';
 import { EyeSlashIcon } from '@heroicons/react/24/solid'
 
-const Question = ({ quiz }) => {
+const Question = ({ quiz, positiveScore, setPositiveScore, negativeScore, setNegativeScore }) => {
     const { question, options, correctAnswer } = quiz;
+
     const handleButtonClick = (answer) => {
         if (JSON.stringify(answer) === JSON.stringify(correctAnswer)) {
+            setPositiveScore(positiveScore + 1);
             toast.success('The Answer is correct!', { autoClose: 1000 })
         }
         else {
+            setNegativeScore(negativeScore < 0 ? 0 : negativeScore + 1);
             toast.error('Your Answer is wrong! Try Again.', { autoClose: 1000 })
         }
     }
@@ -18,11 +21,11 @@ const Question = ({ quiz }) => {
     }
     return (
         <div
-            className="md:w-7/12 sm:w-11/12 flex rounded-lg border border-violet-900 shadow-lg bg-gray-200">
+            className="sm:w-11/12 flex rounded-lg drop-shadow-2xl bg-gray-200">
             <div className="bg-white p-8 rounded-lg shadow-lg w-full">
                 <div className='flex items-center gap-4'>
                     <div>
-                        <h5 className="mb-4 text-xl font-semibold tracking-tight text-violet-900">{question.slice(3, -4)}</h5>
+                        <h5 className="mb-4 text-xl font-semibold text-start tracking-tight text-violet-900">{question.slice(3, -4)}</h5>
                     </div>
                     <div>
                         <EyeSlashIcon onClick={() => checkCorrectAnswer(correctAnswer)} className='w-6 h-6 text-violet-900 cursor-pointer' />
@@ -32,7 +35,7 @@ const Question = ({ quiz }) => {
                 <div className="flex mb-3">
                     <div class="flex flex-col gap-4" role="group">
                         {
-                            options.map((input, idx) => <button key={idx} onClick={(event) => handleButtonClick(event.target.innerText)} className="block mt-2 border border-violet-900 rounded-lg py-2 px-6 hover:bg-violet-400 cursor-pointer">{input}</button>)
+                            options.map((input, idx) => <button key={idx} onClick={(event) => handleButtonClick(event.target.innerText)} className="block mt-2 border border-violet-900 rounded-lg py-2 px-6 hover:bg-violet-200 cursor-pointer">{input}</button>)
                         }
                     </div>
                 </div>
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Question from '../Question/Question';
 
 const Questions = () => {
     const questions = useLoaderData();
     const { name } = questions.data;
+    const [positiveScore, setPositiveScore] = useState(0);
+    const [negativeScore, setNegativeScore] = useState(0);
 
     console.log(questions);
     return (
-        <div className='p-4 flex flex-col justify-center items-center gap-4'>
-            <h1 className='font-bold text-5xl text-center text-indigo-700 mb-4'>Quiz of {name}</h1>
-            {
-                questions.data.questions.map(quiz => <Question
-                    key={quiz.id}
-                    quiz={quiz}
-                >
-                </Question>)
-            }
+        <div className='md:grid grid-cols-2'>
+            <div className='p-4 flex flex-col justify-center items-center gap-4'>
 
+                <h1 className='font-bold text-5xl text-center text-indigo-700 mb-4'>Quiz of {name}</h1>
+                {
+                    questions.data.questions.map(quiz => <Question
+                        key={quiz.id}
+                        quiz={quiz}
+                        positiveScore={positiveScore}
+                        setPositiveScore={setPositiveScore}
+                        negativeScore={negativeScore}
+                        setNegativeScore={setNegativeScore}
+                    >
+                    </Question>)
+                }
+
+
+            </div>
+
+            <div className="h-screen">
+                <div className="w-80 mt-24 m-auto lg:mt-16 max-w-sm">
+                    <img src="https://image.freepik.com/free-vector/app-development-illustration_52683-47931.jpg" alt="" className="rounded-t-2xl shadow-2xl lg:w-full 2xl:w-full 2xl:h-44 object-cover" />
+                    <div className="bg-white shadow-2xl rounded-b-3xl pb-6">
+                        <h2 className="text-center text-gray-800 text-2xl font-bold pt-6">Score Result</h2>
+                        <div className="w-5/6 m-auto">
+                            <p className="text-center text-gray-500 pt-5">Try to do your best little by little. And, it will help you to acheieve your goal.</p>
+                        </div>
+                        <div className="flex justify-center gap-8 w-72  m-auto bg-indigo-50 mt-5 p-4 rounded-2xl">
+                            <div>
+                                <p className="text-gray-800 font-bold lg:text-sm">Right Answer:</p>
+                            </div>
+                            <div>
+                                <p className="text-indigo-700 text-sm font-bold">{positiveScore}</p>
+                            </div>
+                        </div>
+                        <div className="flex justify-center gap-8 w-72  m-auto bg-indigo-50 mt-5 p-4 rounded-2xl">
+                            <div>
+                                <p className="text-gray-800 font-bold lg:text-sm">Wrong Answer:</p>
+                            </div>
+                            <div>
+                                <p className="text-indigo-700 text-sm font-bold">{negativeScore}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
